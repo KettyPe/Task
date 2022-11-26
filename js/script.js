19d101183c8e69081e040fba5a647731a6b776c0
Yuliya <narutoworkzumaki@gmail.com>
2022-11-27 02:01:46 +0700

    Add validate, pravki: bem,image,js, hover

--- js/script.js
+++ js/script.js
@@ -1,84 +1,78 @@
-function send(event, php){
-    console.log("Отправка запроса");
-    event.preventDefault ? event.preventDefault() : event.returnValue = false;
-    var req = new XMLHttpRequest();
-    req.open('POST', php, true);
-    req.onload = function() {
-        if (req.status >= 200 && req.status < 400) {
-            json = JSON.parse(this.response); // Ебанный internet explorer 11
-            console.log(json);
-            
-            // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
-            if (json.result == "success") {
-                // Если сообщение отправлено
-                alert("Сообщение отправлено");
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    const form = document.getElementById ('form');
+    form.addEventListener('submit', formSend);
+
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+
+        let formData = new FormData(form);
+
+        if (error===0) {
+            form.classList.add('_sending');
+            let response = await fetch ('send.php', {
+                method: 'POST',
+                body: formData
+            });
+            if (response.ok) {
+                let result = await response.json();
+                form.classList.remove('_sending');
+                formPreview.innerHTML = '';
+                form.reset();
             } else {
-                // Если произошла ошибка
-                alert("Ошибка. Сообщение не отправлено");
+                alert("Ошибка");
+                form.classList.remove('_sending');
             }
-        // Если не удалось связаться с php файлом
-        } else {alert("Ошибка сервера. Номер: "+req.status);}}; 
-    
-    // Если не удалось отправить запрос. Стоит блок на хостинге
-    req.onerror = function() {alert("Ошибка отправки запроса");};
-    req.send(new FormData(event.target));
-    }
-
-// inputMask
-let inputs = document.querySelectorAll('input[type="tel"]');
-let im = new Inputmask('+7 (999) 999-99-99');
-im.mask(inputs);
-
-/* let form = document.querySelector('.js-form'),
-    formInputs = document.querySelectorAll('.js-input'),
-    inputEmail = document.querySelector('.js-input-email'),
-    inputPhone = document.querySelector('.js-input-phone')
-
-
-function validateEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-
-function validateCountry(country) {
-    let re = new RegExp('.co$');
-    return re.test(String(country).toLowerCase());
-}
-
-form.onsubmit = function () {
-    let emailVal = inputEmail.value,
-        phoneVal = inputPhone.value,
-        emptyInputs = Array.from(formInputs).filter(input => input.value === '');
-
-    formInputs.forEach(function (input) {
-        if (input.value === '') {
-            input.classList.add('error');
-
-        } else {
-            input.classList.remove('error');
         }
-    });
-
-    if (emptyInputs.length !== 0) {
-        console.log('inputs not filled');
-        return false;
     }
 
-    if(!validateEmail(emailVal)) {
-        console.log('email not valid');
-        inputEmail.classList.add('error');
-        return false;
-    } else {
-        inputEmail.classList.remove('error');
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if(emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            } else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+            
+        }
+        return error;
     }
 
-    if (validateCountry(emailVal)) {
-        console.log('email from Columbia');
-        inputEmail.classList.add('error');
-        return false;
-    } else {
-        inputEmail.classList.remove('error');
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('_error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.remove('_error');
     }
 
-} */
+    function emailTest(input) {
+        return !/^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i .test(input.value);
+    }
+
+
+    let inputs = document.querySelectorAll('input[type="tel"]');
+    let im = new Inputmask('+7 (999) 999-99-99');
+    im.mask(inputs);
+
+});
+
+
